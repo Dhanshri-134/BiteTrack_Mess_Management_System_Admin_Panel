a6b4c4beb8305d8d534af9514ebd6340f058a966
Dhanshri-134 <yewankardhanshri@gmail.com>
2026-01-14 03:30:19 +0530

    Staff Creation

--- pages/api/billing/status.js
+++ pages/api/billing/status.js
@@ -2,6 +2,13 @@ import { requireAuth } from "../../../lib/requireAuth";
 import { pgPool } from "../../../lib/db";
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   const user = await requireAuth(req, res);
   if (!user) return;
 
