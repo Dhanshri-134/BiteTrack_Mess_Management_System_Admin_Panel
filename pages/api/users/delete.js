1a1ef4e1845135f0a7f2b95de5365e27811f15f6
Dhanshri-134 <yewankardhanshri@gmail.com>
2026-01-22 15:31:03 +0530

    API RESOLVING

--- pages/api/users/delete.js
+++ pages/api/users/delete.js
@@ -28,7 +28,7 @@ import jwt from "jsonwebtoken";
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
   if (req.method === "OPTIONS") {
