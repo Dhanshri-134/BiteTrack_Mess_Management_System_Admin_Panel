05fed99b1d9b649b3101b80b429ec76f969457ce
Dhanshri-134 <yewankardhanshri.com>
2026-03-04 10:49:48 +0530

    regiter api

--- pages/api/register.js
+++ pages/api/register.js
@@ -88,7 +88,20 @@ export default async function handler(req, res) {
   }
   if (req.method !== 'POST') return res.status(405).end();
 
-  const { name, email, phone, mess_id: bodyMessId } = req.body;
+  const {
+  first_name,
+  last_name,
+  name,
+  email,
+  phone,
+  mobile,
+  room_no,
+  hostel_name,
+  course,
+  date_of_joining,
+  gender,
+  food_preference
+} = req.body;
   if (!name || !email) return res.status(400).json({ error: 'Missing fields' });
 
   // ⬅️ Extract mess_id from token (if token exists)
@@ -111,23 +124,80 @@ export default async function handler(req, res) {
 
   // FINAL mess_id logic
   // priority: token → frontend → default(1)
-  const finalMessId = tokenMessId || bodyMessId || 1;
 
   const client = await pgPool.connect();
   try {
     await client.query('BEGIN');
 
     const insert = await client.query(
-      `INSERT INTO users (name, email, phone, mess_id,mail_sent)
-       VALUES ($1,$2,$3,$4,TRUE)
-       ON CONFLICT (email) DO UPDATE SET name=EXCLUDED.name
-       RETURNING id`,
-      [name, email, phone || null, messId]
-    );
+  `INSERT INTO users
+  (
+    first_name,
+    last_name,
+    name,
+    email,
+    phone,
+    alternate_contact,
+    room_no,
+    hostel_name,
+    course,
+    date_of_joining,
+    gender,
+    food_preference,
+    mess_id,
+    mail_sent
+  )
+  VALUES
+  ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,TRUE)
 
-    const userId = insert.rows[0].id;
+  ON CONFLICT (email, mess_id)
+  DO UPDATE SET
+    first_name = EXCLUDED.first_name,
+    last_name = EXCLUDED.last_name,
+    name = EXCLUDED.name,
+    phone = EXCLUDED.phone,
+    alternate_contact = EXCLUDED.alternate_contact,
+    room_no = EXCLUDED.room_no,
+    hostel_name = EXCLUDED.hostel_name,
+    course = EXCLUDED.course,
+    date_of_joining = EXCLUDED.date_of_joining,
+    gender = EXCLUDED.gender,
+    food_preference = EXCLUDED.food_preference
+
+  RETURNING id`,
+  [
+    first_name || null,
+    last_name || null,
+    name,
+    email,
+    phone || null,
+    mobile || null,
+    room_no || null,
+    hostel_name || null,
+    course || null,
+    date_of_joining || null,
+    gender || null,
+    food_preference || null,
+    messId
+  ]
+);
+
+
+const userId = insert.rows[0].id;
+await client.query(
+  `INSERT INTO parents (user_id, name, contact, address)
+   VALUES ($1,$2,$3,$4)`,
+  [
+    userId,
+    req.body.parent_name || null,
+    req.body.parent_contact || null,
+    req.body.parent_address || null
+  ]
+);
     const code = Math.floor(100000 + Math.random() * 900000).toString();
     const expiresAt = new Date(Date.now() + 1000 * 60 * 60);
+
+
 
     await client.query(
       `INSERT INTO verification_codes (user_id, code, expires_at)
